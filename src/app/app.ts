185cf601ea1b153414e41c90b836477159ce9499
Alexey Popov <ASDAlexey@yandex.ru>
2026-07-05 22:25:49 +0300

    feat: migrate app shell to material toolbar and buttons

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component, DOCUMENT, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 import { CREATOR_SITE_URL, HOME_PAGE_LINK, MAIN_CONTENT_ID, RACES_LIST_PAGE_LINK, VK_COMMUNITY_URL } from './app.constant';
@@ -7,7 +9,7 @@ import { RECORDS_PAGE_LINK } from './features/records/records-page.constant';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
